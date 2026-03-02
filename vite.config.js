a08fc03b5c0ff7ccd386bed20c2e5128f5da3a20
liujw <liujw@zhuziplay.com>
2026-03-02 18:46:11 +0800

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 关键：base 设为仓库名（如果仓库名是 jsx-github-pages，就写这个）
+  base: '/BalaTiger.github.io/' 
 })
